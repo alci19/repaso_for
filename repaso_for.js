607d09eced36e012116c942bb995ea19bef05d90
alci19 <bytonalci@gmail.com>
2026-05-27 17:34:48 -0500

    Ejercicio 1

---
+++ repaso_for.js
@@ -0,0 +1,18 @@
+function mostrar(opcion) {
+
+    switch(opcion) {
+
+        case 1:
+            ejercicio1();
+        break;
+
+        default:
+            alert("Opción no válida");
+        break;
+    }
+
+}
+
+function ejercicio1() {
+    alert("Ejercicio 1 ejecutado");
+}
