8b27812b4feac7fefb2becc5292a08c448fc3ba6
alci19 <bytonalci@gmail.com>
2026-05-29 01:49:57 -0500

    Agregamos tabla del 3 y subimos archivo de audio solicitado en el taller

--- repaso_for.js
+++ repaso_for.js
@@ -14,6 +14,34 @@ function mostrar(opcion) {
             ejercicio3();
         break;
 
+        case 4:
+            imprimirCentenas();
+        break;
+
+        case 5:
+            imprimirCentenasRegresivo();
+        break;
+
+        case 6:
+            mostrarMensaje1();
+        break;
+
+        case 7:
+            mostrarMensaje2();
+        break;
+
+        case 8:
+            mostrarMensaje3();
+        break;
+
+        case 9:
+            mostrarMensaje4();
+        break;
+
+        case 10:
+            tablaDel3();
+        break;
+
         default:
             console.log("Opción no válida");
         break;
@@ -50,6 +78,74 @@ function ejercicio3() {
     for(let i = 1; i <= 10; i++) {
 
         console.log("Número: " + i);
+
+    }
+
+}
+function imprimirCentenas() {
+
+    for(let i = 100; i <= 1000; i += 100) {
+
+        console.log(i);
+
+    }
+
+}
+function imprimirCentenasRegresivo() {
+
+    for(let i = 1000; i >= 800; i -= 100) {
+
+        console.log(i);
+
+    }
+
+}
+
+function mostrarMensaje1() {
+
+    for(let i = 5; i < 8; i++) {
+
+        console.log("Mensaje 1");
+
+    }
+
+}
+
+function mostrarMensaje2() {
+
+    for(let i = 10; i >= 8; i--) {
+
+        console.log("Mensaje 2");
+
+    }
+
+}
+
+function mostrarMensaje3() {
+
+    for(let i = 0; i < 3; i++) {
+
+        console.log("Mensaje 3");
+
+    }
+
+}
+
+function mostrarMensaje4() {
+
+    for(let i = 8; i > 5; i--) {
+
+        console.log("Mensaje 4");
+
+    }
+
+}
+
+function tablaDel3() {
+
+    for(let i = 1; i <= 10; i++) {
+
+        console.log("3x" + i + "=" + (3 * i));
 
     }
 
